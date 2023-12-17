b572caf04dc7d1378b85ede9b90a3b8d97a219dc
TheMethier <106596805+TheMethier@users.noreply.github.com>
2023-12-17 20:59:27 +0100

    Implement ShoppingCart

---
+++ backend/_RoBotland/Controllers/ShoppingCartController.cs
@@ -0,0 +1,66 @@
+﻿using _RoBotland.Helpers;
+using _RoBotland.Interfaces;
+using _RoBotland.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace _RoBotland.Controllers
+{
+    [Route("/api/v1/shoppingCart/[controller]")]
+    [ApiController]
+    public class ShoppingCartController: ControllerBase
+    {
+        private IShoppingCartService _shoppingCartService;
+        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        [HttpPost("/add")]
+        public IActionResult AddProductToShoppingCart([FromBody] ProductDto product)//Na froncie egzekwuj mapowanie Product na ProductDto
+        {
+            var session = HttpContext.Session;
+            var context = SessionHelper.GetObjectFromJson<List<ShoppingCartItem>>(session, "shoppingcart");
+            if(context == null)
+            {
+                context = new List<ShoppingCartItem>();
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "shoppingcart", context);
+            }
+            var shoppingCartContent =_shoppingCartService.AddItemToShoppingCart(product,context);
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "shoppingcart", shoppingCartContent);
+            return Ok(shoppingCartContent);
+        }
+        [HttpGet("/actual")]
+        public IActionResult GetShoppingCartItems()
+        {
+            var session = HttpContext.Session;
+            var context = SessionHelper.GetObjectFromJson<List<ShoppingCartItem>>(session, "shoppingcart");
+            if (context == null) return NoContent();
+            return Ok(context);
+        }
+        [HttpDelete("/remove")]
+        public IActionResult RemoveProductFromShoppingCard([FromBody] ProductDto product) 
+        {
+            var session = HttpContext.Session;
+            var context = SessionHelper.GetObjectFromJson<List<ShoppingCartItem>>(session, "shoppingcart");
+            if (context == null)
+            {
+                return NoContent();
+            }
+            try
+            {
+                var shoppingCartContent = _shoppingCartService.RemoveItemFromShoppingCart(product, context);
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "shoppingcart", shoppingCartContent);
+                return Ok(shoppingCartContent);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);//dodaj ify na errory
+            }
+
+        }
+
+
+    }
+}
---
+++ backend/_RoBotland/Services/ShoppingCartService.cs
@@ -0,0 +1,52 @@
+﻿using _RoBotland.Helpers;
+using _RoBotland.Interfaces;
+using _RoBotland.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.IdentityModel.Tokens;
+
+namespace _RoBotland.Services
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        private readonly IMapper _mapper;
+        public ShoppingCartService(IMapper mapper) {
+            _mapper = mapper;
+        }
+        public List<ShoppingCartItem> AddItemToShoppingCart(ProductDto product, List<ShoppingCartItem> shoppingCart)
+        {
+            if (shoppingCart.IsNullOrEmpty())
+            {
+                shoppingCart.Add(new ShoppingCartItem(0, product, 1, product.Price));
+                return shoppingCart;
+            }
+            var identicalItem = shoppingCart.FirstOrDefault(x => x.Product.Name == product.Name);
+            if (identicalItem == null)
+                shoppingCart.Add(new ShoppingCartItem(shoppingCart.Count(),product,1,product.Price));
+            else
+            {
+                shoppingCart[identicalItem.Id].Quantity++;
+                shoppingCart[identicalItem.Id].Total += product.Price;
+            }
+            return shoppingCart;
+        }
+
+   
+        public List<ShoppingCartItem> RemoveItemFromShoppingCart(ProductDto product, List<ShoppingCartItem> shoppingCart)
+        {
+            if(shoppingCart.IsNullOrEmpty())
+                throw new Exception("Empty card"); 
+            var identicalItem = shoppingCart.FirstOrDefault(x => x.Product.Name == product.Name);
+            if (identicalItem == null)
+                throw new Exception("Not found");
+            if (identicalItem.Quantity == 1)
+                shoppingCart.Remove(identicalItem);
+            else
+            {
+                shoppingCart[identicalItem.Id].Quantity--;
+                shoppingCart[identicalItem.Id].Total -= product.Price;
+            }
+            return shoppingCart;
+        }
+    }
+}
